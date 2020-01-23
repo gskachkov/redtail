7cc1a397e74f22949af1014221f7ca056f43c22d
Oleksander Skachkov <gskachkov@gmail.com>
2020-01-23 23:56:55 +0200

    Update px4_controller.py

--- ros/packages/holder/scripts/px4_controller.py
+++ ros/packages/holder/scripts/px4_controller.py
@@ -11,7 +11,7 @@ from mavros_msgs.msg import RCIn
 #from sensor_msgs.msg import NavSatFix
 
 readyToOpen = True
-global joyPub 
+joyPub = True
 # def callback(data):
 #    global readyToOpen
 #    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', len(data.data))
@@ -22,16 +22,18 @@ global joyPub
 #        rospy.loginfo(rospy.get_caller_id() + 'I am droping package')
 
 def callbackRCIn(data):
+    global joyPub
     rospy.loginfo(rospy.get_caller_id() + 'I header all %s', data.channels);
     #rospy.loginfo(rospy.get_caller_id() + 'I header 6 %s', data.channels[6]);
     #rospy.loginfo(rospy.get_caller_id() + 'I header 5 %s', data.channels[5]);
     #rospy.loginfo(rospy.get_caller_id() + 'I header 4 %s', data.channels[4]);
     if data.channels[6] = 1514 :
         joyMsg = Joy()
-        joyMsg.buttons[0] = 1
-        joyPub.publish(joy)
+        joyMsg.buttons = [ 0, 1]
+        joyPub.publish(joyMsg)
 
 def listener():
+    global joyPub
     rospy.init_node('rc_listener_node')
     mavros.set_namespace('mavros')
     joyPub = rospy.Publisher('/joy', Joy)
