10cb0513b0cdd580f2e1c1a027ac2a451268f567
Oleksander Skachkov <gskachkov@gmail.com>
2020-01-23 23:50:50 +0200

    Update px4_controller.py

--- ros/packages/holder/scripts/px4_controller.py
+++ ros/packages/holder/scripts/px4_controller.py
@@ -11,7 +11,7 @@ from mavros_msgs.msg import RCIn
 #from sensor_msgs.msg import NavSatFix
 
 readyToOpen = True
-
+global joyPub 
 # def callback(data):
 #    global readyToOpen
 #    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', len(data.data))
@@ -21,15 +21,20 @@ readyToOpen = True
 #        Popen(["python3", "/home/alex/holder/ServoKit/open_holder.py"], close_fds=True)
 #        rospy.loginfo(rospy.get_caller_id() + 'I am droping package')
 
-def callbackRCIn(data): 
+def callbackRCIn(data):
     rospy.loginfo(rospy.get_caller_id() + 'I header all %s', data.channels);
-    rospy.loginfo(rospy.get_caller_id() + 'I header 6 %s', data.channels[6]);
-    rospy.loginfo(rospy.get_caller_id() + 'I header 5 %s', data.channels[5]);
-    rospy.loginfo(rospy.get_caller_id() + 'I header 4 %s', data.channels[4]);
+    #rospy.loginfo(rospy.get_caller_id() + 'I header 6 %s', data.channels[6]);
+    #rospy.loginfo(rospy.get_caller_id() + 'I header 5 %s', data.channels[5]);
+    #rospy.loginfo(rospy.get_caller_id() + 'I header 4 %s', data.channels[4]);
+    if data.channels[6] = 1514 :
+        joyMsg = Joy()
+        joyMsg.buttons[0] = 1
+        joyPub.publish(joy)
 
 def listener():
+    rospy.init_node('rc_listener_node')
     mavros.set_namespace('mavros')
-    #rospy.Subscriber('/joy', Joy, callback)
+    joyPub = rospy.Publisher('/joy', Joy)
     rospy.Subscriber(mavros.get_topic('rc/in'), RCIn, callbackRCIn)
 
     # spin() simply keeps python from exiting until this node is stopped
