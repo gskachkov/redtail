ecead215064353ed9c3ab9780bea718c239ffa38
Skachkov Oleksandr <gskachkov@gmail.com>
2020-01-24 23:52:32 +0200

    Update px4_controler

--- ros/packages/holder/scripts/px4_controller.py
+++ ros/packages/holder/scripts/px4_controller.py
@@ -12,6 +12,9 @@ from mavros_msgs.msg import RCIn
 
 readyToOpen = True
 joyPub = True
+switch_on = True
+switch_off = True
+
 # def callback(data):
 #    global readyToOpen
 #    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', len(data.data))
@@ -21,22 +24,15 @@ joyPub = True
 #        Popen(["python3", "/home/alex/holder/ServoKit/open_holder.py"], close_fds=True)
 #        rospy.loginfo(rospy.get_caller_id() + 'I am droping package')
 
-def getJoyMessage():
+def getJoyMessage(on, off):
     msg = Joy()
-    msg.axes = [
+    msg.axes = [0,0,0,0,0,0,]
+    msg.buttons = [
+            on,
+            off,
             0,
             0,
-            0,
-            0,
-            0,
-            0,
-        ]
-        msg.buttons = [
-            0,
-            0,
-            0,
-            0,
-            0,
+            1,
             0,
             0,
             0,
@@ -51,25 +47,30 @@ def getJoyMessage():
             0,
             0,
         ]
-     return msg
+    return msg
 
 def callbackRCIn(data):
     global joyPub
+    global switch_on
+    global switch_off
     rospy.loginfo(rospy.get_caller_id() + 'I header all %s', data.channels);
-    #rospy.loginfo(rospy.get_caller_id() + 'I header 6 %s', data.channels[6]);
-    #rospy.loginfo(rospy.get_caller_id() + 'I header 5 %s', data.channels[5]);
-    #rospy.loginfo(rospy.get_caller_id() + 'I header 4 %s', data.channels[4]);
-    if data.channels[6] = 1514 :
-        joyMsg = Joy()
-                
-        joyMsg.buttons = [ 0, 1]
+    rospy.loginfo(rospy.get_caller_id() + 'I header ' +  str(switch_on) + '-' + str(switch_off));
+    if data.channels[6] == 2003 and switch_on:
+        joyMsg = getJoyMessage(1, 0)        
         joyPub.publish(joyMsg)
+        switch_on = False
+        switch_off = True
+    elif data.channels[6] == 1024 and switch_off:
+        joyMsg = getJoyMessage(0, 1)
+        joyPub.publish(joyMsg)
+        switch_off = False
+        switch_on = True
 
 def listener():
     global joyPub
     rospy.init_node('rc_listener_node')
     mavros.set_namespace('mavros')
-    joyPub = rospy.Publisher('/joy', Joy)
+    joyPub = rospy.Publisher('/joy', Joy, queue_size=1)
     rospy.Subscriber(mavros.get_topic('rc/in'), RCIn, callbackRCIn)
 
     # spin() simply keeps python from exiting until this node is stopped
