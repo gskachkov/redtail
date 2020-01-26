4fab40f321af07f15fa8367df6fa4fadbe1f8783
Oleksandr Skachkov <gskachkov@gmail.com>
2020-01-26 18:39:54 +0200

    Create task for ros

--- ros/packages/holder/scripts/listener.py
+++ ros/packages/holder/scripts/listener.py
@@ -53,17 +53,10 @@ def callback(data):
         rospy.loginfo(rospy.get_caller_id() + 'I am droping package')
 
 def listener():
-
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # name are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
     rospy.init_node('listener', anonymous=True)
 
     rospy.Subscriber('/object_dnn/network/output', Image, callback)
 
-    # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
--- ros/packages/holder/scripts/px4_controller.py
+++ ros/packages/holder/scripts/px4_controller.py
@@ -5,76 +5,63 @@ from std_msgs.msg import String
 from sensor_msgs.msg import Image, Joy
 from subprocess import Popen
 import mavros
-#from geometry_msgs.msg import PoseStamped
-from mavros_msgs.msg import RCIn 
-#from mavros_msgs.srv import CommandBool, SetMode
-#from sensor_msgs.msg import NavSatFix
+from mavros_msgs.msg import RCIn
 
-readyToOpen = True
+packageHolded = True
 joyPub = True
 switch_on = True
 switch_off = True
 
-# def callback(data):
-#    global readyToOpen
-#    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', len(data.data))
-#    if len(data.data) > 1 and readyToOpen:
-#        rospy.loginfo(rospy.get_caller_id() + 'I see person first time')
-#        readyToOpen = False
-#        Popen(["python3", "/home/alex/holder/ServoKit/open_holder.py"], close_fds=True)
-#        rospy.loginfo(rospy.get_caller_id() + 'I am droping package')
+
+def callbackDnn(data):
+    global packageHolded
+    if len(data.data) > 1 and packageHolded:
+        rospy.loginfo(rospy.get_caller_id() + 'I see person first time and droping package')
+        packageHolded = False
+        # Move path to open holder command to variable. It works with Python3, so we run it from outside ros 
+        Popen(["python3", "/home/alex/holder/ServoKit/open_holder.py"], close_fds=True)
+
 
 def getJoyMessage(on, off):
     msg = Joy()
-    msg.axes = [0,0,0,0,0,0,]
-    msg.buttons = [
-            on,
-            off,
-            0,
-            0,
-            1,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-            0,
-        ]
+    msg.axes = [0, 0, 0, 0, 0, 0]
+    msg.buttons = [on, off, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
     return msg
+
 
 def callbackRCIn(data):
     global joyPub
     global switch_on
     global switch_off
-    rospy.loginfo(rospy.get_caller_id() + 'I header all %s', data.channels);
-    rospy.loginfo(rospy.get_caller_id() + 'I header ' +  str(switch_on) + '-' + str(switch_off));
+    # rospy.loginfo(rospy.get_caller_id() + 'I header all %s', data.channels);
+
     if data.channels[6] == 2003 and switch_on:
         joyMsg = getJoyMessage(1, 0)        
         joyPub.publish(joyMsg)
         switch_on = False
         switch_off = True
+        rospy.loginfo(rospy.get_caller_id() + 'I header: start dnn ')
     elif data.channels[6] == 1024 and switch_off:
         joyMsg = getJoyMessage(0, 1)
         joyPub.publish(joyMsg)
         switch_off = False
         switch_on = True
+        rospy.loginfo(rospy.get_caller_id() + 'I header: stop dnn ');
+
 
 def listener():
     global joyPub
     rospy.init_node('rc_listener_node')
     mavros.set_namespace('mavros')
+
     joyPub = rospy.Publisher('/joy', Joy, queue_size=1)
+  
     rospy.Subscriber(mavros.get_topic('rc/in'), RCIn, callbackRCIn)
+    rospy.Subscriber('/object_dnn/network/output', Image, callbackDnn)
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
+
 
 if __name__ == '__main__':
     listener()
