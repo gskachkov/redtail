d7fb844b53291b01de24a02500aacc59fe3df0f5
Oleksandr Skachkov <gskachkov@gmail.com>
2020-01-27 21:00:04 +0200

    Update buttons

--- ros/packages/holder/scripts/holder_controller.py
+++ ros/packages/holder/scripts/holder_controller.py
@@ -13,15 +13,6 @@ switch_on = True
 switch_off = True
 
 
-def callbackDnn(data):
-    global packageHolded
-    if len(data.data) > 1 and packageHolded:
-        rospy.loginfo(rospy.get_caller_id() + 'I see person first time and droping package')
-        packageHolded = False
-        # Move path to open holder command to variable. It works with Python3, so we run it from outside ros 
-        Popen(["python3", "/home/alex/holder/ServoKit/open_holder.py"], close_fds=True)
-
-
 def getJoyMessage(on, off):
     msg = Joy()
     msg.axes = [0, 0, 0, 0, 0, 0]
@@ -33,7 +24,7 @@ def callbackRCIn(data):
     global joyPub
     global switch_on
     global switch_off
-    #rospy.loginfo(rospy.get_caller_id() + 'I header all %s', data.channels);
+    rospy.loginfo(rospy.get_caller_id() + 'I header all %s', data.channels)
 
     if data.channels[6] == 2003 and switch_on:
         joyMsg = getJoyMessage(1, 0)        
@@ -52,11 +43,11 @@ def callbackRCIn(data):
 def listener():
     global joyPub
     rospy.init_node('holder_contoller_node')
-    rospy.loginfo(rospy.get_caller_id() + "Start work" )
+    rospy.loginfo(rospy.get_caller_id() + "Start work")
     mavros.set_namespace('mavros')
 
     joyPub = rospy.Publisher('/joy', Joy, queue_size=1)
-  
+
     rospy.Subscriber(mavros.get_topic('rc/in'), RCIn, callbackRCIn)
     rospy.Subscriber('/object_dnn/network/output', Image, callbackDnn)
 
--- ros/packages/holder/scripts/listener.py
+++ ros/packages/holder/scripts/listener.py
@@ -1,62 +1,25 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
-
-## Simple talker demo that listens to std_msgs/Strings published 
-## to the 'chatter' topic
 
 import rospy
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from subprocess import Popen
 
-readyToOpen = True
+packageHolded = True
 
-def callback(data):
-    global readyToOpen
-    rospy.loginfo(rospy.get_caller_id() + 'I heard %s', len(data.data))
-    if len(data.data) > 1 and readyToOpen:
-        rospy.loginfo(rospy.get_caller_id() + 'I see person first time')
-        readyToOpen = False
+
+def callbackDnn(data):
+    global packageHolded
+    if len(data.data) > 1 and packageHolded:
+        rospy.loginfo(rospy.get_caller_id() + 'I see person first time and droping package')
+        packageHolded = False
+        # Move path to open holder command to variable. It works with Python3, so we run it from outside ros 
         Popen(["python3", "/home/alex/holder/ServoKit/open_holder.py"], close_fds=True)
-        rospy.loginfo(rospy.get_caller_id() + 'I am droping package')
+
 
 def listener():
     rospy.init_node('listener', anonymous=True)
-
     rospy.Subscriber('/object_dnn/network/output', Image, callback)
-
     rospy.spin()
 
 if __name__ == '__main__':
